98d52d02d4fcc083b2fd5f4ca95ceb166c735c9d
AL <be26427@gmail.com>
2022-04-03 16:14:47 -0400

    project creation

---
+++ src/main/java/guru/springboot/spring5frameworkrecipe/Spring5FrameworkRecipeApplication.java
@@ -0,0 +1,13 @@
+package guru.springboot.spring5frameworkrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5FrameworkRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5FrameworkRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springboot/spring5frameworkrecipe/Spring5FrameworkRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springboot.spring5frameworkrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5FrameworkRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
